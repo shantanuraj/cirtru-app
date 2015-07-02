71a5e9bea7486acc6990bbd3e49bd3685bcc2396
Shantanu Raj <s@sraj.me>
2015-07-02 07:08:05 +0530

    Cache cookie on login, reuse if no cookie present.

--- app/store/LocalStorage.js
+++ app/store/LocalStorage.js
@@ -1,17 +1,32 @@
 'use strict';
 
 var AsyncStorage = require('react-native').AsyncStorage,
-    UserActions = require('../actions/UserActions');
+    UserActions = require('../actions/UserActions'),
+    CookieManager = require('react-native-cookies');
 
 var USER = 'User@';
+var COOKIE = 'Cookie@';
 
 var LocalStorage = {
     saveUser(user) {
+        CookieManager.getAll((cookies, res) => {
+            if (cookies) {
+                AsyncStorage.setItem(COOKIE, JSON.stringify(cookies), error => {
+                    if (error) {
+                        console.log('Cookie not saved');
+                    } else {
+                        console.log('Cookie cached', cookies);
+                    }
+                });
+            }
+        });
         AsyncStorage.setItem(USER, JSON.stringify(user), error => {
             if (error) {
                 console.log('Could not save user');
+            } else {
+                console.log('Saved user');
+                UserActions.login(user);
             }
-            UserActions.login(user);
         });
     },
 
@@ -23,12 +38,36 @@ var LocalStorage = {
             } else {
                 console.log('No user present');
             }
-        })
+        });
+        AsyncStorage.getItem(COOKIE)
+        .then(value => {
+            if (value !== null) {
+                CookieManager.getAll((cookies, res) => {
+                    if (!cookies['connect.sid']) {
+                        this.setCookie(value);
+                    }
+                });
+            }
+        });
+    },
+
+    setCookie(value) {
+        var cookies = JSON.parse(value)['connect.sid'];
+        cookies.origin = 'https://cirtru.com';
+        cookies.version = 1;
+        cookies.expiration = '2017-05-30T12:30:00.00-05:00';
+
+        CookieManager.set(cookies, (extra, res) => {
+            if (res === 'success') {
+                console.log('Picked from cache', value);
+            }
+        });
     },
 
     deleteUser() {
         AsyncStorage.removeItem(USER);
+        AsyncStorage.removeItem(COOKIE);
     },
-}
+};
 
 module.exports = LocalStorage;
