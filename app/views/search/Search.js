06a747064dc86836727ed8266911ed8c569dafb7
Shantanu Raj <s@sraj.me>
2015-07-07 07:32:39 +0530

    Reuse everything, make queries for advance car options.

--- app/views/search/Search.js
+++ app/views/search/Search.js
@@ -67,14 +67,17 @@ var Search = React.createClass({
 	getInitialState() {
 		return {
 			searchBox: '',
-			make: '',
-			color: '',
-			year: '',
 
-			locations: [],
+			location: [],
 			showLocation: false,
-			circles: [],
+			circle: [],
 			showCircle: false,
+			year: [],
+			showYear: false,
+			make: [],
+			showMake: false,
+			color: [],
+			showColor: false,
 			
 			showRoomType: false,
 			type: null,
@@ -97,13 +100,13 @@ var Search = React.createClass({
 			showMinMileage: false,
 			minMileage: null,
 			showMaxMileage: false,
-			maxMileage: false,
+			maxMileage: null,
 		};
 	},
 
 	staticQuery(query, option) {
 		return option ? query + '=' + option + '&' : '';
-	}
+	},
 
 	dynamicQuery(query, options) {
 		if (options.length === 0) {
@@ -118,18 +121,24 @@ var Search = React.createClass({
 	buildQueryString() {
 		var query = '';
 		query += this.staticQuery('searchBox', this.state.searchBox);
-		query += this.staticQuery('make', this.state.make);
-		query += this.staticQuery('color', this.state.color);
-		query += this.staticQuery('year', this.state.year);
 		query += this.staticQuery('type', this.state.type);
 		query += this.staticQuery('lowerPrice', this.state.minPrice);
 		query += this.staticQuery('upperPrice', this.state.maxPrice);
+		query += this.dynamicQuery('primaryLocation', this.state.location);
+		query += this.dynamicQuery('circle', this.state.circle);
+
 		query += this.staticQuery('drinking', this.state.drinking);
 		query += this.staticQuery('smoking', this.state.smoking);
 		query += this.staticQuery('eatPreference', this.state.diet);
 		query += this.staticQuery('pets', this.state.pets);
-		query += this.dynamicQuery('primaryLocation', this.state.locations);
-		query += this.dynamicQuery('circle', this.state.circles);
+
+		query += this.staticQuery('titleStatus', this.state.titleStatus);
+		query += this.staticQuery('lowerMileage', this.state.minMileage);
+		query += this.staticQuery('upperMileage', this.state.maxMileage);
+		query += this.dynamicQuery('make', this.state.make);
+		query += this.dynamicQuery('year', this.state.year);
+		query += this.dynamicQuery('color', this.state.color);
+
 		return query;
 	},
 
@@ -145,20 +154,6 @@ var Search = React.createClass({
 		this.setState(state);
 	},
 
-	selectLocation(locations) {
-		this.setState({
-			locations,
-			showLocation: false,
-		});
-	},
-
-	selectCircle(circles) {
-		this.setState({
-			circles,
-			showCircle: false,
-		});
-	},
-
 	selectItem(key, val, visibility) {
 		var state = this.state;
 		state[key] = val;
@@ -172,8 +167,8 @@ var Search = React.createClass({
 
 	renderPromptOrNumber(prompt, info, items) {
 		info = ' ' + info;
-		if (items.length === 1) {
-			info = info.substring(0, info.length - 1);
+		if (items.length > 1) {
+			info += 's';
 		}
 		info += ' selected';
 		return items.length > 0 ? items.length + info: prompt;
@@ -183,6 +178,21 @@ var Search = React.createClass({
 		return (
 			<SinglePicker
 			action={selected => this.selectItem(key, selected, visibility)}
+			isVisible={this.state[visibility]}
+			label={label}
+			list={list} />
+		);
+	},
+
+	renderMultiPicker(label, list, visibility) {
+		return (
+			<Picker
+			action={(list)=> {
+				var state = this.state;
+				state[visibility] = false;
+				state[label] = list
+				this.setState({ state });
+			}}
 			isVisible={this.state[visibility]}
 			label={label}
 			list={list} />
@@ -200,26 +210,42 @@ var Search = React.createClass({
 		);
 	},
 
+	renderMultiPrompt(label, val, visibility) {
+		return (
+			<TouchableOpacity
+			onPress={() => this.showPicker(visibility)}>
+				<Text style={styles.selectText}>
+					{this.renderPromptOrNumber(label, val, this.state[val])}
+				</Text>
+			</TouchableOpacity>
+		);
+	},
+
 	renderRoommateOptions() {
 		return (
 			<View>
 				{this.renderPrompt('Property type', this.state.type, 'showRoomType')}
-				{this.renderPrompt('Minimum price', this.state.minPrice, 'showMinPrice')}
-				{this.renderPrompt('Maximum price', this.state.maxPrice, 'showMaxPrice')}
-				{this.renderPrompt('Minimum mileage', this.state.minMileage, 'showMinMileage')}
-				{this.renderPrompt('Maximum mileage', this.state.maxMileage, 'showMaxMileage')}
-				{this.renderPrompt('Gender preference', this.state.gender, 'showGender')}
-				{this.renderPrompt('Diet preference', this.state.diet, 'showDiet')}
-				{this.renderPrompt('Smoking preference', this.state.smoking, 'showSmoking')}
-				{this.renderPrompt('Drinking preference', this.state.drinking, 'showDrinking')}
-				{this.renderPrompt('Pets', this.state.pets, 'showPets')}
 				{this.renderSinglePicker('type', 'showRoomType', 'property type', Api.roommatesFilterSet.propertyTypes)}				
+
+				{this.renderPrompt('Minimum price', this.state.minPrice, 'showMinPrice')}
 				{this.renderSinglePicker('minPrice', 'showMinPrice', 'minimum price', Api.roommatesFilterSet.minPrice)}
+
+				{this.renderPrompt('Maximum price', this.state.maxPrice, 'showMaxPrice')}
 				{this.renderSinglePicker('maxPrice', 'showMaxPrice', 'maximum price', Api.roommatesFilterSet.maxPrice)}
+
 				{this.renderSinglePicker('gender', 'showGender', 'gender preference', Api.roommatesFilterSet.gender)}
+				{this.renderPrompt('Gender preference', this.state.gender, 'showGender')}
+
+				{this.renderPrompt('Diet preference', this.state.diet, 'showDiet')}
 				{this.renderSinglePicker('diet', 'showDiet', 'diet preference', Api.roommatesFilterSet.diet)}
+
+				{this.renderPrompt('Smoking preference', this.state.smoking, 'showSmoking')}
 				{this.renderSinglePicker('smoking', 'showSmoking', 'smoking preference', Api.roommatesFilterSet.smoking)}
+
+				{this.renderPrompt('Drinking preference', this.state.drinking, 'showDrinking')}
 				{this.renderSinglePicker('drinking', 'showDrinking', 'drinking preference', Api.roommatesFilterSet.drinking)}
+
+				{this.renderPrompt('Pets', this.state.pets, 'showPets')}
 				{this.renderSinglePicker('pets', 'showPets', 'pet preference', Api.roommatesFilterSet.pets)}
 			</View>
 		);
@@ -229,28 +255,40 @@ var Search = React.createClass({
 		return (
 			<View>
 				{this.renderPrompt('Property type', this.state.type, 'showRoomType')}
-				{this.renderPrompt('Minimum price', this.state.minPrice, 'showMinPrice')}
-				{this.renderPrompt('Maximum price', this.state.maxPrice, 'showMaxPrice')}
 				{this.renderSinglePicker('type', 'showRoomType', 'property type', Api.subletsFilterSet.propertyTypes)}
-				{this.renderSinglePicker('minPrice', 'showMinPrice', 'minimum price', Api.subletsFilterSet.minPrice)}
+
+				{this.renderPrompt('Maximum price', this.state.maxPrice, 'showMaxPrice')}
 				{this.renderSinglePicker('maxPrice', 'showMaxPrice', 'maximum price', Api.subletsFilterSet.maxPrice)}
+
+				{this.renderPrompt('Minimum price', this.state.minPrice, 'showMinPrice')}
+				{this.renderSinglePicker('minPrice', 'showMinPrice', 'minimum price', Api.subletsFilterSet.minPrice)}
 			</View>
 		);
 	},
 
 	renderCarOptions() {
+		var options = this.state.filterStore.options[this.props.category];
 		return (
 			<View>
 				{this.renderPrompt('Title status', this.state.titleStatus, 'showTitleStatus')}
-				{this.renderPrompt('Minimum price', this.state.minPrice, 'showMinPrice')}
-				{this.renderPrompt('Maximum price', this.state.maxPrice, 'showMaxPrice')}
-				{this.renderPrompt('Minimum mileage', this.state.minMileage, 'showMinMileage')}
-				{this.renderPrompt('Maximum mileage', this.state.maxMileage, 'showMaxPrice')}
 				{this.renderSinglePicker('titleStatus', 'showTitleStatus', 'title status', Api.carsFilterSet.titleStatus)}
-				{this.renderSinglePicker('minPrice', 'showMinPrice', 'minimum price', Api.carsFilterSet.minPrice)}
+
+				{this.renderPrompt('Maximum price', this.state.maxPrice, 'showMaxPrice')}
 				{this.renderSinglePicker('maxPrice', 'showMaxPrice', 'maximum price', Api.carsFilterSet.maxPrice)}
-				{this.renderSinglePicker('minMileage', 'showMinMileage', 'minimum mileage', Api.carsFilterSet.minMileage)}
+
+				{this.renderPrompt('Minimum price', this.state.minPrice, 'showMinPrice')}
+				{this.renderSinglePicker('minPrice', 'showMinPrice', 'minimum price', Api.carsFilterSet.minPrice)}
+
+
+				{this.renderPrompt('Maximum mileage', this.state.maxMileage, 'showMaxPrice')}
 				{this.renderSinglePicker('maxMileage', 'showMaxMileage', 'maximum mileage', Api.carsFilterSet.maxMileage)}
+
+				{this.renderPrompt('Minimum mileage', this.state.minMileage, 'showMinMileage')}
+				{this.renderSinglePicker('minMileage', 'showMinMileage', 'minimum mileage', Api.carsFilterSet.minMileage)}
+
+				{this.renderMultiPrompt('Make', 'make', 'showMake')}
+				{this.renderMultiPrompt('Color', 'color', 'showColor')}
+				{this.renderMultiPrompt('Years', 'year', 'showYear')}
 			</View>
 		);
 	},
@@ -280,19 +318,15 @@ var Search = React.createClass({
 				placeholderTextColor={Colors.grey}
 				style={styles.searchBar} />
 
-				<TouchableOpacity
-				onPress={() => this.showPicker('showLocation')}>
-					<Text style={styles.selectText}>
-						{this.renderPromptOrNumber('Select Location', 'locations', this.state.locations)}
-					</Text>
-				</TouchableOpacity>
+				{this.renderMultiPrompt('Location', 'location', 'showLocation')}
+				{this.renderMultiPicker('location', options.location, 'showLocation')}
+				
+				{this.renderMultiPrompt('Circle', 'circle', 'showCircle')}
+				{this.renderMultiPicker('circle', options.circle, 'showCircle')}
 
-				<TouchableOpacity
-				onPress={() => this.showPicker('showCircle')}>
-					<Text style={styles.selectText}>
-						{this.renderPromptOrNumber('Select Circle', 'circles', this.state.circles)}
-					</Text>
-				</TouchableOpacity>
+				{this.renderMultiPicker('make', options.make, 'showMake')}
+				{this.renderMultiPicker('year', options.year, 'showYear')}
+				{this.renderMultiPicker('color', options.color, 'showColor')}
 
 				{this.renderNonGenericContent()}
 
@@ -303,18 +337,6 @@ var Search = React.createClass({
 						Search
 					</Text>
 				</TouchableHighlight>
-
-				<Picker
-				action={this.selectLocation}
-				isVisible={this.state.showLocation}
-				label={'location'}
-				list={options.location} />
-
-				<Picker 
-				action={this.selectCircle}
-				isVisible={this.state.showCircle}
-				label={'circle'}
-				list={options.circle} />
 			</View>
 		);
 	},
