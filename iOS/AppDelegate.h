570ae2024241db110070faa37012e93b472b943f
Shantanu Raj <s@sraj.me>
2015-07-08 18:57:22 +0530

    Add Google sign in configuration.

--- iOS/AppDelegate.h
+++ iOS/AppDelegate.h
@@ -8,8 +8,9 @@
  */
 
 #import <UIKit/UIKit.h>
+#import <Google/SignIn.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, GIDSignInDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
