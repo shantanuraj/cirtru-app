a9ae6e8f687ed651db6c015ef77808db860b5ad2
Shantanu Raj <s@sraj.me>
2015-07-14 18:15:55 +0530

    Lint CarInfo.

--- app/views/util/CarInfo.js
+++ app/views/util/CarInfo.js
@@ -10,7 +10,42 @@ var {
     StyleSheet
 } = React;
 
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: window.width,
+        justifyContent: 'center',
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingTop: 16,
+    },
+    title: {
+        fontSize: 16,
+    },
+    value: {
+        fontSize: 16,
+    },
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 2,
+    }
+});
+
 var CarInfo = React.createClass({
+    renderRow(title, value) {
+        if (!value) {
+            return;
+        }
+        return (
+            <View style={styles.row}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.value}>{value}</Text>
+            </View>
+        );
+    },
+
     render() {
         var car = this.props.car,
             make = car.make,
@@ -39,43 +74,6 @@ var CarInfo = React.createClass({
             </View>
         );
     },
-
-    renderRow(title, value) {
-        if (!value) {
-            return;
-        }
-        return (
-            <View style={styles.row}>
-                <Text style={styles.title}>{title}</Text>
-                <Text style={styles.value}>{value}</Text>
-            </View>
-        );
-    },
-});
-
-var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: window.width,
-        height: 400,
-        justifyContent: 'center',
-        backgroundColor: Colors.backgroundLight,
-        padding: 12,
-    },
-    title: {
-        color: Colors.black,
-        fontSize: 20,
-    },
-    value: {
-        color: Colors.grey,
-        fontSize: 20,
-    },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginBottom: 2,
-    }
 });
 
 module.exports = CarInfo;
