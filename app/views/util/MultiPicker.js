c3e4d711e76b2b6438a1aa0e52f741e39ff1a6fc
Shantanu Raj <s@sraj.me>
2015-07-17 20:15:09 +0530

    Add much nicer styling for multipicker.

--- app/views/util/MultiPicker.js
+++ app/views/util/MultiPicker.js
@@ -1,12 +1,15 @@
 'use strict';
 
-let React = require('react-native');
+let React = require('react-native'),
+	Colors = require('../../core/Colors');
 
 let {
+	TouchableHighlight,
+	ScrollView,
 	StyleSheet,
 	ListView,
-	Text,
 	View,
+	Text,
 } = React;
 
 let window = require('Dimensions').get('window');
@@ -14,37 +17,111 @@ let window = require('Dimensions').get('window');
 let styles = {
 	listContainer: {
 		flex: 1,
+		backgroundColor: Colors.grey,
+	},
+
+	container: {
+		backgroundColor: Colors.grey,
+	},
+
+	selectedRow: {
+		backgroundColor: Colors.brandPrimaryDark,
+		width: window.width - 32,
+		padding: 16,
 	},
 
 	selectableRow: {
 		width: window.width,
 		padding: 16,
-		margin: 2,
+		marginBottom: 1,
 	},
 
     leadText: {
     	fontSize: 16,
+    	color: Colors.white,
     },
+
+    buttonContainer: {
+    	backgroundColor: Colors.transparent,
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		height: 70,
+		width: window.width,
+		padding: 16,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	button: {
+		width: window.width - 32,
+		height: 50,
+		padding: 8,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 3,
+		backgroundColor: Colors.brandSecondaryLight,
+		shadowColor: Colors.black,
+        shadowOpacity: 0.5,
+        shadowRadius: 6,
+        shadowOffset: {
+            height: 0,
+            width: 0,
+        },
+	},
+
+	buttonText: {
+		fontSize: 20,
+		color: Colors.white,
+	},
 };
 
 let MultiPicker = React.createClass({
-	renderRow(rowText) {
+	getInitialState() {
+		return {
+			dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+			list: this.props.list.map(item => {
+				return {
+					label: item[this.props.accessKey],
+					selected: false,
+				}
+			}),
+			selected: [],
+		};
+	},
+
+	renderRow(row) {
+		let rowStyle = row.selected ? styles.selectedRow : styles.selectableRow;
 		return (
-			<View style={styles.selectableRow}>
+			<TouchableHighlight
+			underlayColor={Colors.brandPrimaryDark}
+			style={rowStyle}>
 				<Text style={styles.leadText}>
-					{rowText}
+					{row.label}
 				</Text>
-			</View>
+			</TouchableHighlight>
 		);
 	},
 	render() {
-		console.log(this.props);
-		let dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 		return (
-			<ListView
-			style={styles.listContainer}
-			dataSource={dataSource.cloneWithRows(this.props.list)}
-			renderRow={row => this.renderRow(row[this.props.accessKey])} />
+			<View style={{flex: 1}}>
+				<ListView
+				style={styles.listContainer}
+				contentContainerStyle={styles.container}
+				initialListSize={15}
+				dataSource={this.state.dataSource.cloneWithRows(this.state.list)}
+				renderRow={row => this.renderRow(row)} />
+
+				<View style={styles.buttonContainer}>
+					<TouchableHighlight style={styles.button}
+					underlayColor={Colors.brandSecondary}
+					onPress={this.props.action}>
+						<Text style={styles.buttonText}>
+							Done
+						</Text>
+					</TouchableHighlight>
+				</View>
+			</View>
 		);
 	},
 });
